d2d64db8dec24b63b8de8ec3ce286d0f13bd7512
dancroucher <dan.croucher@gmail.com>
2025-01-18 23:09:41 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,17 +4,26 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        Casseetter
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {`* Experience-driven advice and practical assistance on all aspects of game development.
+* Evaluation and troubleshooting of existing development pipelines, teams and projects.
+* Real-world project planning, scheduling, and resourcing.
+* Hands-on, day-to-day Production of game development at any stage, for any duration and for any level of involvement.
+* Providing processes and leadership for any and all phases of game development - concept generation, prototyping, pre-production, production, end-phases and post-launch.
+* Strategic planning for studio growth and high-quality development at a range of team sizes.
+* Advise on hiring, team building, and maintaining studio culture, especially in fully remote teams. 
+* Assisting with management of publisher-client relationships from pitch to post-launch.
+* Creating pitch presentations, including key creative, production and business aspects.
+* Costing projects and creating resource plans for project pitches.
+* Sourcing, costing and running key service providers, especially QA, localisation and user-testing.
+* Building cross-disciplinary teams, and leading other producers and creative staff.
+* Solving problems in creative and technical teams with collaborative leadership.
+* Managing certification testing plans for Sony, Nintendo, and Microsoft platforms.
+* Supporting multi-platform releases across PC, console, and mobile.`}
       </p>
       <div className="my-8">
-        <BlogPosts />
       </div>
     </section>
   )
