cc503d680120c5ea9b5c465c971016301e7a90b3
dancroucher <dan.croucher@gmail.com>
2025-01-21 21:12:20 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
       <h1 className="mb-8 text-8xl font-bold tracking-tighter">
         Cassetter
       </h1>
-      <h2 className="mb-4" text-4xl font-bold tracking-tighter">
+      <h2 className="mb-4 text-4xl font-bold tracking-tighter">
         Bespoke Video Game Production
       </h2>
       <div className="my-8">
