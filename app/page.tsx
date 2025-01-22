d2b6b335831c9a84b1defdf534128fae1e0a3f35
dancroucher <dan.croucher@gmail.com>
2025-01-22 09:05:48 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,15 @@ export default function Page() {
       </h2>
       <div className="my-8">
         <p>
-          <ul>
+          Cassetter is me, <a href="https://www.linkedin.com/in/dancroucher/">Dan Croucher</a> 
+          I'm a creative and versatile video game producer and studio leader with 20+ years of hands-on game industry experience.<br>
+          I've worked with big budgets and large teams, on a shoestring with tiny ones, and everything in-between. 
+          I've shipped many commercially and critically acclaimed games across a large range of platforms, on time and on budget.<br>
+          I pride myself on my drive, passion, attention to detail, straightforward approach and pursuit of quality - both at a game development and studio management level.
+          <br>
+          I can help you with a range of Production work in video game development, just get in touch below for a chat.
+          </br>
+          <ul style="list-style-type: '◾ ';" >
             <li>Coffee</li>
             <li>Tea</li>
             <li>Milk</li>
