3095510ab9abbd6129a1f9ddf0abd1b7a5c0868e
dancroucher <dan.croucher@gmail.com>
2025-01-22 13:50:01 +0000

    Create services.tsx

---
+++ app/services.tsx
@@ -0,0 +1,55 @@
+import { BlogPosts } from 'app/components/posts'
+
+export default function Page() {
+  return (
+    <section>
+      <h1 className="mb-8 text-8xl font-bold tracking-tighter">
+        Cassetter
+      </h1>
+      <h2 className="mb-4 text-4xl font-bold tracking-tighter">
+        Bespoke Video Game Production
+      </h2>
+          <p className="prose">&nbsp;</p>
+          <p className="prose">Cassetter is me, <a href="https://www.linkedin.com/in/dancroucher/">Dan Croucher</a>.</p>
+          <p className="prose">&nbsp;</p>
+          <p className="prose">I'm a creative and versatile video game producer and studio leader with 20+ years of hands-on game industry experience.</p>
+          <p className="prose">&nbsp;</p>
+          <p className="prose">
+          Making games is hard, and a producer is critical for successful development. 
+          </p>
+          <p className="prose">&nbsp;</p>
+          <p className="prose">
+          You may only want someone in your team for a limited time. Or just some experienced eyes on the important numbers and dates at the end of your pitch. Or maybe you're from outside of games and don't know where to start.
+          </p>
+          <p className="prose">&nbsp;</p>
+          <p className="prose">
+          The right person in production can get things done. Fix an unhappy team or build one. Rescue a blown budget or write one from scratch. Make development easier, faster and happier. 
+          </p>
+          <p className="prose">&nbsp;</p>
+          <p className="prose">
+          I can help in these situations, and many more:
+          </p>
+          <p className="prose">
+            <ul>
+              <li>Experience-driven advice and practical assistance on all aspects of game development.</li>
+              <li>Evaluation and troubleshooting of existing development pipelines, teams and projects.</li>
+              <li>Real-world project planning, scheduling, and resourcing.</li>
+              <li>Finding developmental methods to ship on time, on budget, and at high quality.</li>
+              <li>Hands-on, day-to-day production of game development at any stage, for any duration and level of involvement.</li>
+              <li>Providing processes and leadership for any and all phases of game development - concept generation, prototyping, pre-production, production, endphases and post-launch.</li>
+              <li>Strategic planning for studio growth and high-quality development at a range of team sizes.</li>
+              <li>Advise on hiring, team building, and maintaining studio culture, especially in fully remote teams.</li>
+              <li>Assisting with management of publisher-client relationships from pitch to post-launch.</li>
+              <li>Creating pitch presentations, including key creative, production and business aspects.</li>
+              <li>Costing projects and creating resource plans for project pitches.</li>
+              <li>Sourcing, costing and running key service providers, especially QA, localisation and user-testing.</li>
+              <li>Structuring and building cross-disciplinary teams.</li>
+              <li>Leading producers, QA, and creative staff.</li>
+              <li>Solving problems in creative and technical teams with collaborative leadership.</li>
+              <li>Managing certification planning for Sony, Nintendo, and Microsoft platforms.</li>
+              <li>Supporting multi-platform releases across PC, console, and mobile.</li>
+            </ul>
+          </p>
+    </section>
+  )
+}
