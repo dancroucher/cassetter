d34632d90c3d67bccfcc4a33f26189204435e512
dancroucher <dan.croucher@gmail.com>
2025-01-22 09:18:37 +0000

    fixing

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { BlogPosts } from 'app/components/posts'
+// import { BlogPosts } from 'app/components/posts'
 
 export default function Page() {
   return (
@@ -10,6 +10,21 @@ export default function Page() {
         Bespoke Video Game Production
       </h2>
       <div className="my-8">
+        <p>
+          Cassetter is me, <a href="https://www.linkedin.com/in/dancroucher/">Dan Croucher</a> 
+          I'm a creative and versatile video game producer and studio leader with 20+ years of hands-on game industry experience.<br>
+          I've shipped many commercially and critically acclaimed games across a large range of platforms, on time and on budget.<br>
+          I pride myself on my drive, passion, attention to detail, straightforward approach and pursuit of quality.
+          <br>
+          Making games is hard, and a producer is critical for successful development.<br>You may only need someone in your team for a limited time. Or some experienced eyes on the important numbers and dates at the end of your pitch. A specialist can assist in fixing an unhappy team, a blown budget or dates you can't hit.<br>
+          I can help in all of these situations, and many more. Just get in touch below for a chat.
+          <br>
+        </p>
+        <ul>
+          <li>Coffee</li>
+          <li>Tea</li>
+          <li>Milk</li>
+        </ul>
       </div>
     </section>
   )
