62b39f8172cfbf83088ab77f997193e0f05884eb
dancroucher <dan.croucher@gmail.com>
2025-01-23 10:24:40 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,27 @@
 import { BlogPosts } from 'app/components/posts'
 import { Navbar } from './components/nav'
 
+function show_image(src, width, height,alt) {
+  // Create a new image element
+  let img = document.createElement("img");
+
+  // Set the source, width, 
+  // height, and alt attributes
+  img.src = src;
+  img.width = width;
+  img.height = height;
+  img.alt = alt;
+
+  // Append the image element
+  // to the body of the document
+  document.body.appendChild(img);
+}
+
+// Example usage:
+show_image(
+"https://media.geeksforgeeks.org/wp-content/uploads/20231228172727/gfg-image.jpg",
+  300, 200,"gfg logo");
+  
 export default function Page() {
   return (
     <section>
