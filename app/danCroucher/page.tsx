c18d20cbefdcf781dc940b57dc578456101b809e
dancroucher <dan.croucher@gmail.com>
2025-01-27 15:30:23 +0000

    Update page.tsx

--- app/danCroucher/page.tsx
+++ app/danCroucher/page.tsx
@@ -14,20 +14,18 @@ export default function DanCroucher() {
           className="mr-4"
         /></a>
         <h1 className="text-8xl font-bold tracking-tighter">Cassetter</h1>
-        </div>
-  
-      <p className="prose mb-4 flex items-center">
-
+      </div>
+      <div className="prose mb-4 flex items-center">
         <Image
-            src={danCroucherPic}
-            width={180}
-            height={240}
-            alt=""
-            className="ml-4"
-          />
-                <h2 className="mb-4 text-4xl font-bold tracking-tighter">Dan Croucher</h2>
-                <h3 className="mb-2 text-2xl font-semibold tracking-tight">Dev Director, Exec Producer, Producer</h3>    
-      </p>
+              src={danCroucherPic}
+              width={180}
+              height={240}
+              alt=""
+              className="ml-4"
+            />
+        <h2 className="mb-4 text-4xl font-bold tracking-tighter">Dan Croucher</h2>
+        <h3 className="mb-2 text-2xl font-semibold tracking-tight">Dev Director, Exec Producer, Producer</h3>    
+      </div>
       <span>
           I'm a creative and versatile video game producer and studio leader with 20+ years of hands-on game industry experience. I've worked with big budgets and large teams, on a shoestring with tiny ones, and everything in-between. 
           <br /><br />
