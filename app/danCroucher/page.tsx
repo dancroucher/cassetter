94ddc221cc7eb9db8cb609a3c7dc36e9201173be
dancroucher <dan.croucher@gmail.com>
2025-01-27 15:35:50 +0000

    Update page.tsx

--- app/danCroucher/page.tsx
+++ app/danCroucher/page.tsx
@@ -14,24 +14,25 @@ export default function DanCroucher() {
           className="mr-4"
         /></a>
         <h1 className="text-8xl font-bold tracking-tighter">Cassetter</h1>
-      </div>
-      <div className="flex items-center">
-        <Image
-              src={danCroucherPic}
-              width={180}
-              height={240}
-              alt=""
-              className="ml-4"
-            />
-        <h2 className="mb-4 text-4xl font-bold tracking-tighter">Dan Croucher</h2>
-        <h3 className="mb-2 text-2xl font-semibold tracking-tight">Dev Director, Exec Producer, Producer</h3>    
-      </div>
-      <span>
+        </div>
+      <h2 className="mb-4 text-4xl font-bold tracking-tighter">Dan Croucher</h2>
+      <h3 className="mb-2 text-2xl font-semibold tracking-tight">Dev Director, Exec Producer, Producer</h3>      
+      <p className="prose mb-4 flex items-center">
+        <span>
           I'm a creative and versatile video game producer and studio leader with 20+ years of hands-on game industry experience. I've worked with big budgets and large teams, on a shoestring with tiny ones, and everything in-between. 
           <br /><br />
           I've shipped many commercially and critically acclaimed games across a large range of platforms, on time and on budget. I pride myself on my drive, passion, attention to detail, straightforward approach and pursuit of quality - both at a game development and studio management level.
         </span>
-        <br /><br />
+        <Image
+            src={danCroucherPic}
+            width={180}
+            height={240}
+            alt=""
+            className="ml-4"
+          />
+      </p>
+
+
       <h3 className="mb-4 text-2xl font-bold tracking-tighter">Things I can do</h3>
       <p className="prose">
         <ul>
