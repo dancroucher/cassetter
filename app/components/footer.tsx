7a450c34e8215d2b6e8c043d805d7212188ce658
dancroucher <dan.croucher@gmail.com>
2025-01-28 09:02:36 +0000

    Update footer.tsx

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -24,7 +24,7 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="mailto:info@cassetter.com"
+            href="mailto:dan@cassetter.com"
           >
             <ArrowIcon />
             <p className="ml-2 h-7">email</p>
